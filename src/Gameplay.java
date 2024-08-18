71e7295800bc7ea4f69bda760f40b183fa7ad46b
suthikawong <suthika.wsrd@gmail.com>
2024-08-18 17:39:55 +0700

    draw board

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -6,27 +6,56 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Gameplay extends JPanel implements KeyListener, ActionListener {
-    private Color backgroundColor;
     private Color borderColor;
     private int borderSize;
+
+    // board
+    private Color backgroundColor;
+    private int boardSize;
+    private int gridRowColumn;
     private int gridSize;
+    private int gridThickness;
+    private Color gridColor;
 
     public Gameplay() {
         backgroundColor = new Color(40, 40, 40);
         borderColor = Color.BLACK;
         borderSize = 10;
-        gridSize = 20;
+        gridRowColumn = 20;
+        gridThickness = 2;
+        gridColor = Color.GRAY;
 
-        // border
+        // board
         setBackground(backgroundColor);
-        setBorder(BorderFactory.createLineBorder(borderColor, borderSize));
+//        setBorder(BorderFactory.createLineBorder(borderColor, borderSize));
+
+
     }
 
     @Override
     public void paint(Graphics g) {
-        // board
+        // draw board
         super.paintComponent(g);
-        super.printBorder(g);
+
+        int panelWidth = getWidth();
+        int panelHeight = getHeight();
+
+        boardSize = Math.min(panelWidth, panelHeight) - borderSize * 2;
+        gridSize = boardSize / gridRowColumn;
+
+        int gridOffsetX = (panelWidth - gridRowColumn * gridSize) / 2;
+        int gridOffsetY = (panelHeight - gridRowColumn * gridSize) / 2;
+
+        g.setColor(gridColor);
+        for (int i = 0; i <= gridRowColumn; i++) {
+            int yPos = gridOffsetY + i * gridSize;
+            g.drawLine(gridOffsetX, yPos, gridOffsetX + boardSize - 1, yPos);
+        }
+
+        for (int j = 0; j <= gridRowColumn; j++) {
+            int xPos = gridOffsetX + j * gridSize;
+            g.drawLine(xPos, gridOffsetY, xPos, gridOffsetY + boardSize - 1);
+        }
 
         g.dispose();
     }
